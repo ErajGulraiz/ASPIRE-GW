8b92d63643f69a5b4db466f527888d232110486c
eraj-khan <erajj94@gmail.com>
2021-06-19 20:44:02 +0500

    The main script file using the other modules for the whole project (made for my own convenience)

---
+++ main.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import csv
+import matplotlib.mlab as mlab
+import numpy as np
+import math
+import random
+from scipy import misc
+
+from ranbh import *
+
+
+from step1 import *
+
+
+
+def main():
+
+
+	if __name__ == '__main__':
+    		main()
